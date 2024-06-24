cae2c29619c0df536fdd41caaabf108d6714fa80
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2024-06-24 05:28:14 +0300

    make build

---
+++ index.f518e494.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.position="static",t.style.display="block",e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center";
+//# sourceMappingURL=index.f518e494.js.map
